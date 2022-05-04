b9eaeed3e3654ccf183c6a6d72085a0caa5d3464
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2022-05-04 11:52:55 +0200

    Attribute Binding

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data()  {
+    return {
+      product: 'Sword',
+      infoProduct: 'Shop',
+      logo: '/assets/images/vue.svg',
+      image: './assets/images/1.png',
+
+    }
+  }
+})
+
+
+app.mount('#app')
