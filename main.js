2a16157e32cba61d4f1a05381eca769a736a825d
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2022-05-10 11:47:39 +0200

    Computed Properties

--- main.js
+++ main.js
@@ -6,21 +6,33 @@ const app = Vue.createApp({
       product: 'Sword',
       infoProduct: 'Shop',
       logo: '/assets/images/vue.svg',
-      image: './assets/images/1.png',
-      inStock: true,
-      inventory: 2,
+      // image: './assets/images/1.png',
+      selectedVariant: 0, // 0 bedziemy aktualizować po indeksie variantu
+      // inStock: true,
+      inventory: 30,
       onSale: true,
       
       details: ['80% steel', '12% wood', '8% leather'],
       variants: [
-        {id: 1111, color: 'violet', name: 'Short Sword', image: './assets/images/1.png'},
-        {id: 1112, color: 'red', name: 'Sabre', image: './assets/images/2.png'},
-        {id: 1113, color: 'blue', name: 'Pirate Sabre', image: './assets/images/3.png'},
-        {id: 1114, color: 'green', name: 'spars', image: './assets/images/4.png'}
+        {id: 1111, color: 'violet', name: 'Short Sword', image: './assets/images/1.png', quantity: 50},
+        {id: 1112, color: 'red', name: 'Sabre', image: './assets/images/2.png', quantity: 0},
+        {id: 1113, color: 'blue', name: 'Pirate Sabre', image: './assets/images/3.png', quantity: 10},
+        {id: 1114, color: 'green', name: 'spars', image: './assets/images/4.png', quantity: 500}
       ],
       sizes: ['short', 'middle', 'long']
 
 
+    }
+  },
+  computed: {
+    title() {
+      return this.product + ' ' + this.infoProduct
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
     }
   },
   methods: {
@@ -30,10 +42,10 @@ const app = Vue.createApp({
     resetCart() {
       this.cart = 0
     },
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
+      console.log(index)
     }
-
   }
 })
 
